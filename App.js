8cc54d3c09c39e8150f21f0e1124eaeac76bdebb
HitPokar <20ce083@charusat.edu.in>
2021-09-27 09:40:41 +0530

    first-commit

--- App.js
+++ App.js
@@ -4,19 +4,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './Screen/LoginScreen';
 import ScannerScreen from './Screen/ScannerScreen';
 
-
 const Stack = createStackNavigator();
 const screenOptions = {
   headerShown: false,
 };
 
-
-
 export default function App() {
-  return (<NavigationContainer>
-    <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Scanner" component={ScannerScreen} />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Scanner" component={ScannerScreen} />
       </Stack.Navigator>
-  </NavigationContainer>);
+    </NavigationContainer>
+  );
 }
